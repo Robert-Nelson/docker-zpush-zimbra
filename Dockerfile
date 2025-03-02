8d034c7cead44ed49575759873ee6703c4ec5676
Robert Nelson <robert-git@nelson.house>
2025-03-02 11:01:17 -0800

    Rewrite Dockerfile and updatee to PHP 7.4, Z-Push 2.7.5, Zimbra 73

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,56 @@
-FROM php:7.0-apache
-MAINTAINER Serge Ohl <docker@vizuweb.fr>
+FROM php:7.4.33-apache
 
-ENV VERSION 2.3
-ENV VERSIONFULL 2.3.9
-ENV BACKENDVERSION 66
-ENV TERM xterm
+LABEL MAINTAINER="Robert Nelson <robert-zpush@nelson.house>"
 
-ENV ZPUSH_URL zpush_default
+ENV VERSION=2.7
+ENV VERSIONFULL=2.7.5
+ENV BACKENDVERSION=73
+ENV TERM=xterm
+
+ENV ZPUSH_URL=zpush_default
+
+# PHP extensions
+RUN docker-php-source extract
+
+RUN apt-get update && apt-get install -y libzip-dev zlib1g && \
+	docker-php-ext-configure zip && \
+	docker-php-ext-install zip
+
+RUN apt-get install -y \
+		libxml2-dev \
+		libicu-dev
+
+RUN apt-get install -y libmemcached-dev \
+	&& pecl install memcached \
+	&& docker-php-ext-enable memcached
+	
+RUN	docker-php-ext-install -j$(nproc) soap intl
+
+RUN	docker-php-source delete
 
 # Install zpush
-RUN cd /var/www/html && \
-	curl -L "http://download.z-push.org/final/${VERSION}/z-push-${VERSIONFULL}.tar.gz" | tar --strip-components=1 -x -z 
+RUN mkdir /usr/local/lib/z-push/ /var/log/z-push /var/lib/z-push && \
+	chmod 755 /usr/local/lib/z-push/ /var/log/z-push /var/lib/z-push && \
+	chown www-data:www-data -R /usr/local/lib/z-push /var/log/z-push /var/lib/z-push
+	
+RUN curl -L "https://github.com/Z-Hub/Z-Push/archive/refs/tags/$VERSIONFULL.tar.gz" | tar xvz && \
+	mv Z-Push-$VERSIONFULL/src/* /usr/local/lib/z-push/ && \
+	rm -rf Z-Push-$VERSIONFULL
 
 # Add zimbra backend
-RUN cd /var/www/html/backend  && \
+RUN cd /usr/local/lib/z-push/backend  && \
 	curl -o zpzb-install.sh -L "https://sourceforge.net/projects/zimbrabackend/files/Release${BACKENDVERSION}/zpzb-install.sh/download"  && \
 	curl -o zimbra${BACKENDVERSION}.tgz  -L "http://downloads.sourceforge.net/project/zimbrabackend/Release${BACKENDVERSION}/zimbra${BACKENDVERSION}.tgz" 
 	
-RUN cd /var/www/html/backend &&\
+RUN cd /usr/local/lib/z-push/backend &&\
 	chmod +x zpzb-install.sh &&\
 	sed -i "/chcon[^']*$/d" zpzb-install.sh &&\
 	/bin/bash ./zpzb-install.sh $BACKENDVERSION ; exit 0
 
-# Create directory for zpush
-RUN mkdir -p /var/log/z-push && mkdir /var/lib/z-push
-RUN chown www-data:www-data -R /var/lib/z-push /var/log/z-push /var/www/html
-
 # Add vhost for zpush
 COPY default-vhost.conf /etc/apache2/sites-enabled/000-default.conf
+
+# RUN apt-get install -y less vim net-tools tcpdump
 
 # Expose Apache
 EXPOSE 80
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,73 @@
 #!/bin/sh +x
 
-ZPUSH_CONFIG='/var/www/html/config.php'
+ZPUSH_CONFIG='/usr/local/lib/z-push/config.php'
+ZPUSH_BACKEND_CONFIG='/usr/local/lib/z-push/backend/zimbra/config.php'
+ZPUSH_AUTODISCOVER_CONFIG='/usr/local/lib/z-push/autodiscover/config.php'
 
 #Prepare config file
 
 if [ ! -f  "${ZPUSH_CONFIG}.bkp" ]; then
     # Backun original file
-    cp $ZPUSH_CONFIG $ZPUSH_CONFIG.bkp
-     cat <<- EOF >> $ZPUSH_CONFIG
-     #Zimbra config
-    define('MAPI_SERVER', 'file:///var/run/zarafa');
-    define('ZPUSH_URL', '');
-    define('ZIMBRA_URL','');
-    define('ZIMBRA_USER_DIR','zimbra');
-    define('ZIMBRA_SYNC_CONTACT_PICTURES', true);
-    define('ZIMBRA_VIRTUAL_CONTACTS',true);
-    define('ZIMBRA_VIRTUAL_APPOINTMENTS',true);
-    define('ZIMBRA_VIRTUAL_NOTES',true);
-    define('ZIMBRA_VIRTUAL_TASKS',true);
-    define('ZIMBRA_IGNORE_EMAILED_CONTACTS',true);
-    define('ZIMBRA_HTML',true);
-    define('ZIMBRA_ENFORCE_VALID_EMAIL',false);
-    define('ZIMBRA_SMART_FOLDERS',false);
+    if [ -f "$ZPUSH_CONFIG" ]; then
+        cp $ZPUSH_CONFIG $ZPUSH_CONFIG.bkp
+    fi
+fi
+
+if [ ! -f  "${ZPUSH_CONFIG}" ]; then
+    cat <<- EOF >> $ZPUSH_CONFIG
+# Zimbra config
+define('BACKEND_PROVIDER', '');
+define('TIMEZONE','');
+define('ZIMBRA_USER_DIR','zimbra');
+define('ZIMBRA_SYNC_CONTACT_PICTURES', true);
+define('ZIMBRA_VIRTUAL_CONTACTS',true);
+define('ZIMBRA_VIRTUAL_APPOINTMENTS',true);
+define('ZIMBRA_VIRTUAL_NOTES',true);
+define('ZIMBRA_VIRTUAL_TASKS',true);
+define('ZIMBRA_IGNORE_EMAILED_CONTACTS',true);
+define('ZIMBRA_HTML',true);
+define('ZIMBRA_ENFORCE_VALID_EMAIL',false);
+define('ZIMBRA_SMART_FOLDERS',false);
 EOF
 fi
 
 if [ -z  $ZPUSH_URL_IN_HTTP ]; then
     ZPUSH_URL_PROTO="https:\/\/"
 else
-     ZPUSH_URL_PROTO="http:\/\/"
+    ZPUSH_URL_PROTO="http:\/\/"
 fi 
 
 if [ -z  $ZIMBRA_URL_IN_HTTP ]; then
     ZIMBRA_URL_PROTO="https:\/\/"
 else
-     ZIMBRA_URL_PROTO="http:\/\/"
+    ZIMBRA_URL_PROTO="http:\/\/"
 fi 
 
 # Config Zimbra backend
 sed -i "/BACKEND_PROVIDER/s/'[^']*'/'BackendZimbra'/2" $ZPUSH_CONFIG
-sed -i "/ZPUSH_URL/s/'[^']*'/'$ZPUSH_URL_PROTO$ZPUSH_URL\/Microsoft-Server-ActiveSync'/2" $ZPUSH_CONFIG
-sed -i "/ZIMBRA_URL/s/'[^']*'/'$ZIMBRA_URL_PROTO$ZIMBRA_URL'/2" $ZPUSH_CONFIG
+sed -i "/BACKEND_PROVIDER/s/'[^']*'/'BackendZimbra'/2" $ZPUSH_AUTODISCOVER_CONFIG
+
+sed -i -e "/ZPUSH_HOST/sx// xx" -e "/ZPUSH_HOST/s/'[^']*'/'$ZPUSH_HOST'/2" $ZPUSH_AUTODISCOVER_CONFIG
+sed -i "/ZIMBRA_URL/s/'[^']*'/'$ZIMBRA_URL_PROTO$ZIMBRA_URL'/2" $ZPUSH_BACKEND_CONFIG
 
 # Set timezone if exist
 if  [ ! -z $TIMEZONE ]; then  
+    rm /etc/localtime -rf
+    ln -fs /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    dpkg-reconfigure --frontend noninteractive tzdata
+
+    TIMEZONE=`echo $TIMEZONE | sed 's:/:\\\/:'`
     sed -i "/TIMEZONE/s/'[^']*'/'$TIMEZONE'/2" $ZPUSH_CONFIG
+    sed -i "/TIMEZONE/s/'[^']*'/'$TIMEZONE'/2" $ZPUSH_AUTODISCOVER_CONFIG
+    sed -i "/USE_FULLEMAIL_FOR_LOGIN/s/false/true/" $ZPUSH_AUTODISCOVER_CONFIG
 fi
 
-apache2-foreground 
+cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
+
+# apache2-foreground 
+
+rm /var/log/apache2/{access.log,error.log,other_vhosts_access.log}
+
+apache2ctl start
+
+/bin/bash
